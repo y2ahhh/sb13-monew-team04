a9b48307b973b940867f72cff3cc004652bd38ef
rndud <rndud008@gmail.com>
2026-08-15 21:32:19 +0900

    chore: MID4-102 CodeRabbit 리뷰 반영

--- src/main/java/com/codeit/sb13/monew/global/config/MDCLoggingInterceptor.java
+++ src/main/java/com/codeit/sb13/monew/global/config/MDCLoggingInterceptor.java
@@ -2,7 +2,6 @@ package com.codeit.sb13.monew.global.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
@@ -11,25 +10,23 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 public class MDCLoggingInterceptor implements HandlerInterceptor {
 
     public static final String REQUEST_ID = "requestId";
     public static final String REQUEST_METHOD = "requestMethod";
     public static final String REQUEST_URI = "requestUri";
     public static final String REQUEST_IP = "requestIp";
-    public static final String REQUEST_ID_HEADER =  "Monew-Request-Id";
-    private final IpAddress ipAddress;
+    public static final String REQUEST_ID_HEADER = "Monew-Request-Id";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        String requestId = UUID.randomUUID().toString().substring(0, 8);
+        String requestId = UUID.randomUUID().toString();
 
         MDC.put(REQUEST_ID, requestId);
         MDC.put(REQUEST_METHOD, request.getMethod());
         MDC.put(REQUEST_URI, request.getRequestURI());
-        MDC.put(REQUEST_IP, ipAddress.getClientIp(request));
+        MDC.put(REQUEST_IP, request.getRemoteAddr());
         response.setHeader(REQUEST_ID_HEADER, requestId);
 
         return true;
