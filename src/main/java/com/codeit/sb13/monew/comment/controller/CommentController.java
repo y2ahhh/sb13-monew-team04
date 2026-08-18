8ade9eaf0d1a7ed5cf006aa75dc2dada94a3518c
hisjeans <his7jeans@gmail.com>
2026-08-19 07:26:58 +0900

    fix: MID4-80 댓글 컨트롤러 return문 수정 및 API 기본 틀 추가

--- src/main/java/com/codeit/sb13/monew/comment/controller/CommentController.java
+++ src/main/java/com/codeit/sb13/monew/comment/controller/CommentController.java
@@ -4,17 +4,27 @@ package com.codeit.sb13.monew.comment.controller;
 import com.codeit.sb13.monew.comment.service.CommentService;
 import com.codeit.sb13.monew.comment.service.dto.CommentDto;
 import com.codeit.sb13.monew.comment.service.dto.CommentRegisterRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comments")
+@Tag(name = "Comment", description = "Comment API")
 public class CommentController {
+
+  private final CommentService commentService;
 
   public ResponseEntity<CommentDto> createComment(@PathVariable String articleId, @Valid @RequestBody CommentRegisterRequest request) {
     // 요청 처리 로직
-    return ResponseEntity.status(HttpStatus.CREATED);
+    return ResponseEntity.status(HttpStatus.CREATED).body(commentService.create(request));
 
   }
 }
