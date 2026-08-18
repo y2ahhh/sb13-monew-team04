387b7c57247c8e4b6771e8aa7325907fd6aa7520
hisjeans <his7jeans@gmail.com>
2026-08-19 08:18:34 +0900

    fix: MID4-80 댓글 생성 컨트롤러 @PostMapping 추가

--- src/main/java/com/codeit/sb13/monew/comment/controller/CommentController.java
+++ src/main/java/com/codeit/sb13/monew/comment/controller/CommentController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class CommentController {
 
   private final CommentService commentService;
 
+  @PostMapping
   public ResponseEntity<CommentDto> createComment(@PathVariable String articleId, @Valid @RequestBody CommentRegisterRequest request) {
     // 요청 처리 로직
     return ResponseEntity.status(HttpStatus.CREATED).body(commentService.create(request));
