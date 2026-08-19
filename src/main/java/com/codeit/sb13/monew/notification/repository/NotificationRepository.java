78c8ea6bbb003b1ec6acdab3b6a83777776eead9
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-20 01:32:31 +0900

    feat: MID4-108 요청자의 미확인 알림만 조회하는 쿼리 메서드 추가

--- src/main/java/com/codeit/sb13/monew/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/sb13/monew/notification/repository/NotificationRepository.java
@@ -3,7 +3,11 @@ package com.codeit.sb13.monew.notification.repository;
 import com.codeit.sb13.monew.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    List<Notification> findByUser_IdAndConfirmedFalse(UUID userId);
+
 }
