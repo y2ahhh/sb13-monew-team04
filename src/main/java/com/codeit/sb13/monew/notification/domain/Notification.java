f7a75143c898e7b0ec9845e0f2d6f1cb2e40fb7c
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-19 12:39:21 +0900

    chore: UUID를 User로 변경

--- src/main/java/com/codeit/sb13/monew/notification/domain/Notification.java
+++ src/main/java/com/codeit/sb13/monew/notification/domain/Notification.java
@@ -1,7 +1,7 @@
 package com.codeit.sb13.monew.notification.domain;
 
 import com.codeit.sb13.monew.global.domain.UpdatedAtEntity;
-//import com.codeit.sb13.monew.user.domain.User;
+import com.codeit.sb13.monew.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -16,12 +16,9 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification extends UpdatedAtEntity {
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private User user;
-
-    @Column(nullable = false)
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(nullable = false)
     private String content;
@@ -36,36 +33,18 @@ public class Notification extends UpdatedAtEntity {
     @Column(nullable = false)
     private boolean confirmed;
 
-//    @Builder
-//    private Notification(User user, String content, UUID resourceId, ResourceType resourceType) {
-//        this.user = user;
-//        this.content = content;
-//        this.resourceId = resourceId;
-//        this.resourceType = resourceType;
-//        this.confirmed = false;
-//    }
-//
-//    public static Notification create(User user, String content, UUID resourceId, ResourceType resourceType) {
-//        return Notification.builder()
-//                .user(user)
-//                .content(content)
-//                .resourceId(resourceId)
-//                .resourceType(resourceType)
-//                .build();
-//    }
-
     @Builder
-    private Notification(UUID userId, String content, UUID resourceId, ResourceType resourceType) {
-        this.userId = userId;
+    private Notification(User user, String content, UUID resourceId, ResourceType resourceType) {
+        this.user = user;
         this.content = content;
         this.resourceId = resourceId;
         this.resourceType = resourceType;
         this.confirmed = false;
     }
 
-    public static Notification create(UUID userId, String content, UUID resourceId, ResourceType resourceType) {
+    public static Notification create(User user, String content, UUID resourceId, ResourceType resourceType) {
         return Notification.builder()
-                .userId(userId)
+                .user(user)
                 .content(content)
                 .resourceId(resourceId)
                 .resourceType(resourceType)
--- src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
+++ src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
@@ -1,5 +1,6 @@
 package com.codeit.sb13.monew.notification.domain;
 
+import com.codeit.sb13.monew.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,15 +14,19 @@ class NotificationTest {
     @DisplayName("알림을 생성하면 confirmed는 기본값으로 false로 설정된다.")
     void 알림_생성() {
         // given
-        UUID userId = UUID.randomUUID();
+        User user = User.builder()
+                .email("test@test.com")
+                .nickname("테스트")
+                .password("pw")
+                .build();
         UUID resourceId = UUID.randomUUID();
 
         // when
-        Notification notification = Notification.create(userId, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
+        Notification notification = Notification.create(user, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
 
         // then
         assertThat(notification.isConfirmed()).isFalse();
-        assertThat(notification.getUserId()).isEqualTo(userId);
+        assertThat(notification.getUser()).isEqualTo(user);
         assertThat(notification.getContent()).isEqualTo("유저 생성 테스트");
         assertThat(notification.getResourceType()).isEqualTo(ResourceType.COMMENT);
         assertThat(notification.getResourceId()).isEqualTo(resourceId);
