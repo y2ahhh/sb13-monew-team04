16c52b288853f302a0f8acbc3c4c461721fa5fdb
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-20 01:42:15 +0900

    feat: MID4-108 알림 확인 처리 API 구현 - PATCH /api/notifications/{notificationId} 단건 확인 API 추가 - PATCH /api/notifications 전체 확인 API 추가 - Monew-Request-User-ID 헤더로 요청자를 식별하도록 처리 - controller/dto에 NotificationResponse 추가

---
+++ src/main/java/com/codeit/sb13/monew/notification/controller/NotificationController.java
@@ -0,0 +1,34 @@
+package com.codeit.sb13.monew.notification.controller;
+
+import com.codeit.sb13.monew.notification.controller.dto.NotificationResponse;
+import com.codeit.sb13.monew.notification.mapper.NotificationMapper;
+import com.codeit.sb13.monew.notification.service.NotificationService;
+import com.codeit.sb13.monew.notification.service.dto.NotificationResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+    private final NotificationMapper mapper;
+
+    @PatchMapping("/{notificationId}")
+    public ResponseEntity<NotificationResponse> confirmNotification(@PathVariable UUID notificationId,
+                                                                    @RequestHeader("Monew-Request-User-ID") UUID userId) {
+        NotificationResult notificationResult = notificationService.confirmNotification(notificationId, userId);
+        return ResponseEntity.ok(mapper.toResponse(notificationResult));
+    }
+
+    @PatchMapping
+    public ResponseEntity<List<NotificationResponse>> confirmAllNotifications(@RequestHeader("Monew-Request-User-ID") UUID userId) {
+        List<NotificationResult> notificationResults = notificationService.confirmAllNotifications(userId);
+        return ResponseEntity.ok(notificationResults.stream().map(mapper::toResponse).toList());
+    }
+}
