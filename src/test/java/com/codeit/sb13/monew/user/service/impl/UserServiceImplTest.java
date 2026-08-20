02d5605a9a39107bbf8c7f9597e02f302e6348f3
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-08-20 16:05:42 +0900

    test: MID4-84 존재하지 않는 userId 시 saveAndFlush 미호출 검증 추가

--- src/test/java/com/codeit/sb13/monew/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/codeit/sb13/monew/user/service/impl/UserServiceImplTest.java
@@ -236,6 +236,7 @@ public class UserServiceImplTest {
     assertThatThrownBy(() -> userServiceImpl.updateNickname(command))
         .isInstanceOf(UserNotFoundException.class);
     verify(userMapper, never()).toUpdateNicknameResult(any());
+    verify(userRepository, never()).saveAndFlush(any(User.class));
 
   }
 
