11ab752cdb64f949eec3087c4cab5c3f395bbeb0
rndud <rndud008@gmail.com>
2026-08-21 01:15:34 +0900

    feat: MID4-150 NAVER 응답 변환 로직 추가

---
+++ src/main/java/com/codeit/sb13/monew/article/service/naver/NaverNewsMapper.java
@@ -0,0 +1,69 @@
+package com.codeit.sb13.monew.article.service.naver;
+
+import com.codeit.sb13.monew.article.domain.ArticleSource;
+import com.codeit.sb13.monew.article.service.dto.CollectedArticle;
+import com.codeit.sb13.monew.global.exception.article.ArticleFetchParseException;
+import org.apache.commons.text.StringEscapeUtils;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class NaverNewsMapper {
+
+    private static final DateTimeFormatter RFC_1123_DATE_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME;
+    private static final String B_TAG_PATTERN = "(?i)</?b>";
+
+    List<CollectedArticle> toCollectedArticles(NaverNewsSearchResponse response) {
+
+        return response.items()
+                .stream()
+                .filter(this::hasLink)
+                .map(this::mapToCollectedArticle)
+                .toList();
+    }
+
+
+    private boolean hasLink(NaverNewsItem item) {
+        return StringUtils.hasText(item.originallink()) || StringUtils.hasText(item.link());
+    }
+
+    private CollectedArticle mapToCollectedArticle(NaverNewsItem item) {
+        return new CollectedArticle(
+                ArticleSource.NAVER,
+                clean(item.title()),
+                clean(item.description()),
+                clean(getOriginalLinkOrFallback(item)),
+                convertPublicationDate(item)
+        );
+    }
+
+    private LocalDateTime convertPublicationDate(NaverNewsItem item) {
+
+        try {
+            return LocalDateTime.parse(item.pubDate(), RFC_1123_DATE_FORMATTER);
+        } catch (Exception e) {
+            throw new ArticleFetchParseException(ArticleSource.NAVER.name(), e);
+        }
+
+    }
+
+    private String getOriginalLinkOrFallback(NaverNewsItem item) {
+        if (StringUtils.hasText(item.originallink())) {
+            return item.originallink();
+        }
+
+        return item.link();
+    }
+
+    private String clean(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        return StringEscapeUtils.unescapeHtml4(value)
+                .replaceAll(B_TAG_PATTERN, "");
+    }
+
+}
