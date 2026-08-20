7ee31d3a0c6a81aead9413714844b4f674dcd71f
rndud <rndud008@gmail.com>
2026-08-20 16:41:22 +0900

    test: MID4-93 최근 작성 댓글 10건 제한 테스트 추가

--- src/test/java/com/codeit/sb13/monew/comment/repository/CommentRepositoryTest.java
+++ src/test/java/com/codeit/sb13/monew/comment/repository/CommentRepositoryTest.java
@@ -84,24 +84,51 @@ class CommentRepositoryTest {
                 .containsExactly("testComment3", "testComment2", "testComment1");
     }
 
-    private void updateCommentCreatedAt(UUID commentId, LocalDateTime createdAt) {
-        em.getEntityManager()
-                .createNativeQuery("UPDATE comments SET created_at = ? WHERE id = ?")
-                .setParameter(1, createdAt)
-                .setParameter(2, commentId)
-                .executeUpdate();
-    }
-
     @Test
-    @Disabled("TODO")
     @DisplayName("10건 초과 시 최신 10건만 반환")
     void _10건_초과_시_최신_10건만_반환() {
         // given
+        User targetUser = new User("test@eamil.com", "testNickname", "testPassword");
+        User otherUser = new User("otherTest@email.com", "otherTestNickname", "otherTestPassword");
+        userRepository.saveAndFlush(targetUser);
+        userRepository.saveAndFlush(otherUser);
+
+        Article article = articleRepository.saveAndFlush(new Article("testTitle", "testContent", "link", LocalDateTime.now(), "source"));
+        Comment firstComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment1"));
+        Comment secondComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment2"));
+        Comment thirdComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment3"));
+        Comment fourComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment4"));
+        Comment fiveComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment5"));
+        Comment sixComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment6"));
+        Comment sevenComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment7"));
+        Comment eightComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment8"));
+        Comment nineComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment9"));
+        Comment tenComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment10"));
+        Comment elevenComment = commentRepository.saveAndFlush(new Comment(article.getId(), targetUser, "testComment11"));
+        commentRepository.saveAndFlush(new Comment(article.getId(), otherUser, "otherTestComment"));
+
+        LocalDateTime baseTime = LocalDateTime.of(2026, 8, 20, 10, 0);
+        updateCommentCreatedAt(firstComment.getId(), baseTime.minusMinutes(10));
+        updateCommentCreatedAt(secondComment.getId(), baseTime.minusMinutes(9));
+        updateCommentCreatedAt(thirdComment.getId(), baseTime.minusMinutes(8));
+        updateCommentCreatedAt(fourComment.getId(), baseTime.minusMinutes(7));
+        updateCommentCreatedAt(fiveComment.getId(), baseTime.minusMinutes(6));
+        updateCommentCreatedAt(sixComment.getId(), baseTime.minusMinutes(5));
+        updateCommentCreatedAt(sevenComment.getId(), baseTime.minusMinutes(4));
+        updateCommentCreatedAt(eightComment.getId(), baseTime.minusMinutes(3));
+        updateCommentCreatedAt(nineComment.getId(), baseTime.minusMinutes(2));
+        updateCommentCreatedAt(tenComment.getId(), baseTime.minusMinutes(1));
+        updateCommentCreatedAt(elevenComment.getId(), baseTime);
+
+        em.clear();
 
         // when
-
+        List<RecentCommentActivityProjection> recentCommentActivities = commentRepository.findRecentCommentActivities(targetUser.getId(), PageRequest.of(0, 10));
 
         // then
+        assertThat(recentCommentActivities)
+                .extracting(RecentCommentActivityProjection::content)
+                .containsExactly("testComment11", "testComment10", "testComment9", "testComment8", "testComment7", "testComment6", "testComment5", "testComment4", "testComment3", "testComment2");
 
 
     }
@@ -147,5 +174,14 @@ class CommentRepositoryTest {
 
 
     }
+
+    private void updateCommentCreatedAt(UUID commentId, LocalDateTime createdAt) {
+        em.getEntityManager()
+                .createNativeQuery("UPDATE comments SET created_at = ? WHERE id = ?")
+                .setParameter(1, createdAt)
+                .setParameter(2, commentId)
+                .executeUpdate();
+    }
+
 
 }
