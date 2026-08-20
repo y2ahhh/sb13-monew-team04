6bfc54d0d5be8a058763d53bd557577490186119
rndud <rndud008@gmail.com>
2026-08-21 01:55:34 +0900

    feat: MID4-150 NAVER 요청 Provider 계약 추가

---
+++ src/main/java/com/codeit/sb13/monew/article/service/naver/NaverNewsSearchRequestProvider.java
@@ -0,0 +1,7 @@
+package com.codeit.sb13.monew.article.service.naver;
+
+import java.util.List;
+
+public interface NaverNewsSearchRequestProvider {
+    List<NaverNewsSearchRequest> getRequests();
+}
