fc4d04562fbe06813c43d72414dbbef5fc910da0
rndud <rndud008@gmail.com>
2026-08-20 14:11:50 +0900

    test: MID4-93 최근 작성 댓글 빈 목록 RED 테스트 추가

---
+++ src/test/java/com/codeit/sb13/monew/comment/repository/CommentRepositoryTest.java
@@ -0,0 +1,108 @@
+package com.codeit.sb13.monew.comment.repository;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Test
+    @DisplayName("작성한 댓글이 없으면 빈 목록 반환")
+    void returns_empty_list_when_user_has_no_comments() {
+
+        // given
+        UUID userId = UUID.randomUUID();
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        // when
+        List<RecentCommentActivityProjection> projections = commentRepository.findRecentCommentActivities(userId, pageRequest);
+
+
+        // then
+        assertThat(projections).isEmpty();
+
+    }
+
+    @Test
+    @Disabled("TODO")
+    @DisplayName("여러 댓글은 createdAt DESC 최신순")
+    void 여러_댓글은_createdAt_DESC_최신순() {
+        // given
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    @Disabled("TODO")
+    @DisplayName("10건 초과 시 최신 10건만 반환")
+    void _10건_초과_시_최신_10건만_반환() {
+        // given
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    @Disabled("TODO")
+    @DisplayName("삭제된 사용자 댓글 제외")
+    void 삭제된_사용자_댓글_제외() {
+        // given
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    @Disabled("TODO")
+    @DisplayName("삭제된 댓글 제외")
+    void 삭제된_댓글_제외() {
+        // given
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    @Disabled("TODO")
+    @DisplayName("삭제된 기사 댓글 제외")
+    void 삭제된_기사_댓글_제외() {
+        // given
+
+        // when
+
+
+        // then
+
+
+    }
+
+}
