f169bedcfb3519eaed0955d1a52c0a191419a243
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-21 15:44:11 +0900

    feat: MID4-115 알림 목록 조회 파라미터 검증 예외 및 에러코드 추가 - ApiErrorCode에 NOTIFICATION_INVALID_LIMIT(NTF_002), NOTIFICATION_INVALID_CURSOR(NTF_003) 추가 - limit, cursor 형식 오류 시 400을 반환하는 예외 클래스 추가 - Collections.singletonMap 사용으로 cursor가 null이어도 예외 생성 시 NPE가 발생하지 않도록 처리

---
+++ src/main/java/com/codeit/sb13/monew/global/exception/notification/NotificationInvalidLimitException.java
@@ -0,0 +1,13 @@
+package com.codeit.sb13.monew.global.exception.notification;
+
+import com.codeit.sb13.monew.global.exception.ApiErrorCode;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class NotificationInvalidLimitException extends NotificationException{
+
+    public NotificationInvalidLimitException(int limit) {
+        super(ApiErrorCode.NOTIFICATION_INVALID_LIMIT, Map.of("limit", limit));
+    }
+}
