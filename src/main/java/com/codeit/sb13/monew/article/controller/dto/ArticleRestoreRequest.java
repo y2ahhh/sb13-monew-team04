190cc83e1350a256c57f2e52d0019d2c20d0333d
rndud <rndud008@gmail.com>
2026-08-25 01:04:08 +0900

    refactor: MID4-171 복구 요청 DTO 스키마 설정 제거

--- src/main/java/com/codeit/sb13/monew/article/controller/dto/ArticleRestoreRequest.java
+++ src/main/java/com/codeit/sb13/monew/article/controller/dto/ArticleRestoreRequest.java
@@ -1,7 +1,6 @@
 package com.codeit.sb13.monew.article.controller.dto;
 
 import com.codeit.sb13.monew.article.s3.service.dto.ArticleRestoreCommand;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertFalse;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,11 +8,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 public record ArticleRestoreRequest(
-        @Schema(description = "복구 시작일", example = "2026-08-23", requiredMode = Schema.RequiredMode.REQUIRED)
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
         @NotNull
         LocalDate from,
-        @Schema(description = "복구 종료일", example = "2026-08-24", requiredMode = Schema.RequiredMode.REQUIRED)
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
         @NotNull
         LocalDate to
