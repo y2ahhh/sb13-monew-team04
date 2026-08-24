1a44c9fd0074f49d8514e5634605ad46f7cb91db
rndud <rndud008@gmail.com>
2026-08-24 23:24:23 +0900

    fix: MID4-170 스케줄링 활성화 중복 제거

--- src/main/java/com/codeit/sb13/monew/Sb13MonewTeam04Application.java
+++ src/main/java/com/codeit/sb13/monew/Sb13MonewTeam04Application.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableScheduling
 public class Sb13MonewTeam04Application {
 
     public static void main(String[] args) {
