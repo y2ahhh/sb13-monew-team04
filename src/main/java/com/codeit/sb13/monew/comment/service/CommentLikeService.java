115e64bc5ddf98154b71634d482e929423bb3ff9
hisjeans <his7jeans@gmail.com>
2026-08-24 21:26:09 +0900

    fix: MID4-175 댓글/좋아요 서비스 인터페이스 @Valid 검증 추가 및 클래스 구현체 @Valid 검증 제거

--- src/main/java/com/codeit/sb13/monew/comment/service/CommentLikeService.java
+++ src/main/java/com/codeit/sb13/monew/comment/service/CommentLikeService.java
@@ -2,10 +2,11 @@ package com.codeit.sb13.monew.comment.service;
 
 import com.codeit.sb13.monew.comment.service.dto.CommentLikeDto;
 import com.codeit.sb13.monew.comment.service.dto.CommentLikeRegisterCommand;
+import jakarta.validation.Valid;
 
 public interface CommentLikeService {
 
-  CommentLikeDto likeComment(CommentLikeRegisterCommand command); // 댓글 좋아요 등록
-  void unlikeComment(CommentLikeRegisterCommand command); // 댓글 좋아요 취소
+  CommentLikeDto likeComment(@Valid CommentLikeRegisterCommand command); // 댓글 좋아요 등록
+  void unlikeComment(@Valid CommentLikeRegisterCommand command); // 댓글 좋아요 취소
 
 }
