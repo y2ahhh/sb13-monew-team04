4efabeb0bd989dc8ea7aae3bd08b27fddc9b8b9f
hisjeans <his7jeans@gmail.com>
2026-08-25 17:11:46 +0900

    refactor: MID4-86 CommentRepository에 커스텀 레포지토리 인터페이스 추가

--- src/main/java/com/codeit/sb13/monew/comment/repository/CommentRepository.java
+++ src/main/java/com/codeit/sb13/monew/comment/repository/CommentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment, UUID> {
+public interface CommentRepository extends JpaRepository<Comment, UUID>, CommentRepositoryCustom {
 
   Optional<Comment> findByIdAndDeletedAtIsNull(UUID commentId);
 
