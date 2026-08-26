51fb49b905b32d99e5e596e7266a0109aec231ed
rndud <rndud008@gmail.com>
2026-08-26 19:17:56 +0900

    docs: MID4-194 활동내역 Swagger 문서화 및 정적 리소스 반영

--- src/main/java/com/codeit/sb13/monew/activity/controller/UserActivityController.java
+++ src/main/java/com/codeit/sb13/monew/activity/controller/UserActivityController.java
@@ -2,6 +2,7 @@ package com.codeit.sb13.monew.activity.controller;
 
 import com.codeit.sb13.monew.activity.service.UserActivityService;
 import com.codeit.sb13.monew.activity.service.dto.UserActivityDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/user-activities")
 @RequiredArgsConstructor
-public class UserActivityController {
+public class UserActivityController implements UserActivityApi {
     private final UserActivityService userActivityService;
 
+    @Override
     @GetMapping("/{userId}")
     public ResponseEntity<UserActivityDto> getUserActivity(@PathVariable UUID userId) {
         return ResponseEntity.ok(userActivityService.userActivity(userId));
