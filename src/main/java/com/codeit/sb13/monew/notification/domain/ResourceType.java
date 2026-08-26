e7aab0a16bdf9f16fb049075200c4598b1ef5a35
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-27 02:01:47 +0900

    fix: MID4-191 ResourceType 직렬화 시 로케일 의존성 제거 - name().toLowerCase() -> name().toLowerCase(Locale.ROOT)로 변경 - JVM 기본 로케일이 터키어(tr)일 경우 대문자 I가 점 없는 ı로 변환되어   "interest"가 아닌 "ınterest"로 직렬화되는 문제를 방지 - 코드 리뷰 반영

--- src/main/java/com/codeit/sb13/monew/notification/domain/ResourceType.java
+++ src/main/java/com/codeit/sb13/monew/notification/domain/ResourceType.java
@@ -2,12 +2,14 @@ package com.codeit.sb13.monew.notification.domain;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.util.Locale;
+
 public enum ResourceType {
     INTEREST,
     COMMENT;
 
     @JsonValue
     public String toJson() {
-        return name().toLowerCase();
+        return name().toLowerCase(Locale.ROOT);
     }
 }
