919cc0186292acde3b85fd2edf0b6d135df4ebae
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-08-26 16:24:20 +0900

    fix: MID4-188 비밀번호 검증 패턴을 프론트엔드 안내와 일치하도록 완화

--- src/main/java/com/codeit/sb13/monew/user/controller/dto/UserCreateRequest.java
+++ src/main/java/com/codeit/sb13/monew/user/controller/dto/UserCreateRequest.java
@@ -12,14 +12,14 @@ public record UserCreateRequest(
     String email,
 
     @NotBlank(message = "닉네임은 필수입니다.")
-    @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
+    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
     String nickname,
 
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     @Pattern(
-        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).+$",
-        message = "비밀번호는 대문자, 소문자, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다."
+        regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).+$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
     )
     String password
 ) {
