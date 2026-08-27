d08e25d6979c05a82f4ad04cc62cb0a88bc0d04f
hisjeans <his7jeans@gmail.com>
2026-08-27 16:09:03 +0900

    refactor: MID4-196 CommentSearchCommand에 필수 검증 추가 및 limit 최소값 설정, idAfter 제거

--- src/main/java/com/codeit/sb13/monew/comment/service/dto/CommentSearchCommand.java
+++ src/main/java/com/codeit/sb13/monew/comment/service/dto/CommentSearchCommand.java
@@ -1,17 +1,24 @@
 package com.codeit.sb13.monew.comment.service.dto;
 
 import com.codeit.sb13.monew.comment.service.CommentOrderBy;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.data.domain.Sort;
 
 public record CommentSearchCommand (
     UUID articleId,
-    CommentOrderBy orderBy,
-    Sort.Direction direction,
+
+    @NotNull CommentOrderBy orderBy,
+
+    @NotNull Sort.Direction direction,
+
     String cursor,
+
     LocalDateTime after,
-    UUID idAfter,
-    int limit,
-    UUID requestUserId
+
+    @Min(1) int limit,
+
+    @NotNull UUID requestUserId
 ){}
